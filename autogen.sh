ff1ac4ebda386d1f87a7752ab7956c8ae6527d43
José Fonseca <jrfonseca@users.sourceforge.net>
2008-10-09 10:53:57 +0000

    Bump version for release.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-aclocal-1.9 \
+aclocal \
 && autoheader \
-&& automake-1.9 --gnu --add-missing \
+&& automake --gnu --add-missing \
 && autoconf \
 && CFLAGS="-Wall -pedantic -g" ./configure --enable-maintainer-mode
 
